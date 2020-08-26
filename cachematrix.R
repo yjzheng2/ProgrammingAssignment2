872b0c63afb4e7ced3a4a5b8b88f118fcad4e3ba
yjzheng2 <james.zheng@ucsf.edu>
2020-08-25 20:43:37 -0700

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setinverse<-function(solve) m<<-solve
+  getinverse<-function() m
+  list(set = set, get = get,
+       setinverse=setinverse,
+       getinverse=getinverse)
 
-}
+} 
 
 
 ## Write a short comment describing this function
